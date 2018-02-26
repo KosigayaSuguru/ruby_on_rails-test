5d0b8da8d51def3b5a18a30b3f3a3da07ded91c2
self2 <self2>
2018-02-26 23:34:30 +0900

    登録機能と更新機能と部分テンプレート追加

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -11,4 +11,39 @@ class TestController < ApplicationController
         @hogehoge2 = ['1','22','333','4444']
         render :locals => { aaaa: 'localsを使ってテンプレートに値渡してるよ（推奨）' }
     end
+
+    # 表示
+    def post_test_get
+        table1last = Table1.last
+        # table1.col1 = "col1だよ"
+        # table1.col2 = "col2だよ"
+
+        table1new = Table1.new
+        table1new.col1 = table1last.col1
+        table1new.col2 = table1last.col2
+
+        # ランダムで部分テンプレートの pertial1 or pertial2 を切り替える
+        pertial = "test/post_test_get_pertial#{rand(2)+1}"
+        logger.debug pertial
+        render :locals => { update_record: table1last, new_record: table1new, pertial: pertial }
+    end
+
+    # 登録用
+    def post_test_post
+        logger.debug "ろぐです"
+        a = params.require(:table1).permit(:col1, :col2)
+        b = Table1.new(a)
+        b.save
+        redirect_to post_test_get_path, :status => 301
+    end
+
+    # 更新用
+    def post_test_patch
+        a = params.require(:table1).permit(:col1, :col2)
+        b = Table1.last
+        b.col1 = a[:col1]
+        b.col2 = a[:col2]
+        b.save
+        redirect_to post_test_get_path, :status => 302
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'test/test', as: 'test'
+  get 'test/post_test', to: 'test#post_test_get', as: 'post_test_get'
+  post 'test/post_test', to: 'test#post_test_post', as: 'post_test_post'
+  patch 'test/post_test', to: 'test#post_test_patch', as: 'post_test_patch'
 end
